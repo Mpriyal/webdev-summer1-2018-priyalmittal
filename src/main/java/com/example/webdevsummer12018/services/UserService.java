cf65ac48d6bc00afb6d790928b3d6720da498976
Priyal Mittal <priyalmittal@Priyals-MacBook-Pro.local>
2018-05-13 19:19:15 -0400

    Added user service

--- src/main/java/com/example/webdevsummer12018/services/UserService.java
+++ src/main/java/com/example/webdevsummer12018/services/UserService.java
@@ -1,9 +1,15 @@
 package com.example.webdevsummer12018.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.webdevsummer12018.models.User;
@@ -18,4 +24,47 @@ public class UserService {
 	public List<User> findAllUsers() {
 		return (List<User>) userRepository.findAll();
 	}
+	
+	@PostMapping("/api/user")
+	public User createUser(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+	
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(@PathVariable("userId") int userId) {
+		Optional<User> data = userRepository.findById(userId);
+		if(data.isPresent()) {
+			return data.get();
+		}
+		else {
+		return null;
+		}
+	}
+	
+	@PutMapping("/api/user/{userId}")
+	public User updateUser(@RequestBody User newUser, @PathVariable("userId") int userId) {
+		Optional<User> data = userRepository.findById(userId);
+		if(data.isPresent()) {
+			User user = data.get();
+			user.setFirstName(newUser.getFirstName());
+			user.setLastName(newUser.getLastName());
+			user.setDateOfBirth(newUser.getDateOfBirth());
+			user.setEmail(newUser.getEmail());
+			user.setUsername(newUser.getUsername());
+			user.setPassword(newUser.getPassword());
+			user.setRole(newUser.getRole());
+			user.setPhone(newUser.getPhone());
+			userRepository.save(user);
+			return user;
+		}
+		else {
+		return null;
+		}
+	}
+	
+	@DeleteMapping("/api/user/{userId}")
+	public void deleteUser(@PathVariable("userId") int userId) {
+		userRepository.deleteById(userId);
+	}
+	
 }
