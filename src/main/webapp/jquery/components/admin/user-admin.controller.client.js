8ec7588981b05cb70b67b05a6a4fc524e134fca0
Priyal Mittal <priyalmittal@Priyals-MacBook-Pro.local>
2018-05-13 22:02:05 -0400

    Modified controller and template and added user services

--- src/main/webapp/jquery/components/admin/user-admin.controller.client.js
+++ src/main/webapp/jquery/components/admin/user-admin.controller.client.js
@@ -1,43 +1,63 @@
-////IIFE
-//(function () {
-//    var $usernameFld, $passwordFld;
-//    var $removeBtn, $editBtn, $createBtn;
-//    var $firstNameFld, $lastNameFld;
-//    var $userRowTemplate, $tbody;
-//    var userService = new AdminUserServiceClient();
-//    $(main);
-//
-//    function main() { … }
-//    function createUser() { … }
-//    function findAllUsers() { … }
-//    function findUserById() { … }
-//    function deleteUser() { … }
-//    function selectUser() { … }
-//    function updateUser() { … }
-//    function renderUser(user) { … }
-//    function renderUsers(users) { … }
-//})(); //anonymous function
+(function () {
 
-(function hello() {
+        jQuery(main);
+        var tbody;
+        var template;
+        var userService = new UserServiceClient();
 
-	jQuery(main);
+        function main() {
+            tbody = $('tbody');
+            template = $('.wbdv-template');
+            $('#wbdv-create').click(createUser);
+            findAllUsers();
+        }
 
-	function main() {
+        function findAllUsers() {
+            userService.findAllUsers().then(renderUsers);
+        }
 
-        var users = [
-			{username: 'bob'},
-			{username: 'charlie'}
-		];
-        var tr = $('.wbdv-template');
+        /**
+		 * This function creates a user when the present input fields are
+		 * populated and the plus-sign button is clicked
+         */
+        function createUser() {
+        	var username = $('#usernameFld').val();
+            var password = $('#passwordFld').val();
+            var firstName = $('#firstNameFld').val();
+            var lastName = $('#lastNameFld').val();
+            var role = $('#roleFld').val();
 
-        var body = $('tbody');
-        for(var i=0;i<users.length;i++){
-        	var user = users[i];
-        	console.log(user);
-            var clone = tr.clone();
-            clone.find('.wbdv-username').html(user.username);
-            body.append(clone);
+            var user = {
+            	username: username,
+				password: password,
+				firstName: firstName,
+				lastName: lastName,
+				role: role
+			};
+
+            userService.createUser(user).then(findAllUsers);
 		}
+
+		function deleteUser(event) {
+            console.log(event);
+        }
+
+        function renderUsers(users) {
+
+            tbody.empty();
+            for (var i = 0; i < users.length; i++) {
+                var user = users[i];
+                var clone = template.clone();
+                clone.find('.wbdv-username').html(user.username);
+                clone.find('.wbdv-password').html(user.password);
+                clone.find('.wbdv-first-name').html(user.firstName);
+                clone.find('.wbdv-last-name').html(user.lastName);
+                clone.find('.wbdv-role').html(user.role);
+                clone.find('.wbdv-remove').click(deleteUser);
+                // clone.find('.wbdv-edit').click(updateUser);
+                tbody.append(clone);
+            }
+        }
+
     }
-}
 )();
---
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -0,0 +1,23 @@
+function UserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.url = 'http://localhost:8080/api/user';
+    var self = this;
+
+    function createUser(user) {
+        return fetch(self.url, {
+            method: 'post',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+    }
+
+    function findAllUsers() {
+        return fetch(self.url)
+        .then(function (response) {
+            return response.json();
+        });
+    }
+}
